fb30e409994b05034ca840641eb0961d08aa4f1e
Cauafanho <110307721+Cauafanho@users.noreply.github.com>
2022-07-30 19:38:49 -0300

    Delete conxtcc.php

--- conxtcc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $server="Localhost";
-    $user="root";
-    $password="Seemg@123";
-    $database="tcc";
-
-    $conn = mysqli_connect($server, $user, $password, $database)or die('erro na conexão!!!');
-
-?>
