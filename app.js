01ddac58c7a2c05af2096bb2c656343c9873903c
Cauafanho <110307721+Cauafanho@users.noreply.github.com>
2022-09-05 11:31:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const vagaSeats = [
+	false,
+	false,
+	false,
+	false,
+	false,
+	false
+];
+
+let busySeats = 0;
+
+const createSeats = (array) => {
+	const containerSeats = document.getElementById("seats");
+
+	
+}
